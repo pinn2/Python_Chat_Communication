ef0d384aedac3e6f52f9dac3f264da2b0c1cd404
pintu sharma <pin2sharma248@gmail.com>
2018-06-26 12:17:05 +0530

    Update Pints.py

--- Pints.py
+++ Pints.py
@@ -6,33 +6,34 @@ import sys
 import time
 
 
-s=socket.socket()
-host = socket.gethostname()
+s=socket.socket()              # socket variable
+host = socket.gethostname()    # getting host name of server
 print (host)
 
-port =8080
-s.bind((host,port))
+port =8080                     # use server 8080 port number for communication
+s.bind((host,port))            # port binded with host to communication
 print ("Successfully connected host and port")
 
-s.listen(1)
+s.listen(1)                    # waiting for 1 number of user to communicate
 print ("Pints waiting for connection")
 
 print (" ")
-
-conn,add = s.accept()
+ 
+conn,add = s.accept()          # if any user connect then it accept connection request return the connection obj->conn and
+                               # add contain host name of user as I.P address and port number
 print (add,"Has connected to Pints")
 
 print (" ")
 while 1:
-        msg = input(str("Pints: "))
-        msg = msg.encode()
-        conn.send(msg)
+        msg = input(str("Pints: "))   # server Pints message to send
+        msg = msg.encode()            # message encoded into bit frame
+        conn.send(msg)                # message send  to user
         print ("Deliverd")
         print (" ")
 
 
-        in_msg = conn.recv(1024)
-        in_msg = in_msg.decode()
+        in_msg = conn.recv(1024)      # message recieved by user
+        in_msg = in_msg.decode()      # decode to print String message
 
         print("User: ",in_msg)
         print (" ")
